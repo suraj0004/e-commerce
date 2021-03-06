29a3ff5b92941e7996dd039e89e9fb82cd6aaaa1
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-03-06 17:51:00 +0530

    add: brand products page

--- app/Http/Controllers/ShopModule/HomeController.php
+++ app/Http/Controllers/ShopModule/HomeController.php
@@ -19,22 +19,30 @@ class HomeController extends Controller
 
     public function contact()
     {
-        return view("shop.contact");
+        return view("shop.contact")->with([
+            "page" => "contact",
+        ]);
     }
 
     //products fn
 
     public function product()
     {
-        return view('shop.product');
+        return view('shop.product')->with([
+            "page" => "product",
+        ]);
     }
     public function cart()
     {
-        return view('shop.cart');
+        return view('shop.cart')->with([
+            "page" => "cart",
+        ]);
     }
     public function show_checkout_page()
     {
-        return view('shop.checkout');
+        return view('shop.checkout')->with([
+            "page" => "show_checkout_page",
+        ]);
     }
 
     public function category()
@@ -45,6 +53,7 @@ class HomeController extends Controller
 
         return view('shop.category')->with([
             'categories' => $categories,
+            "page" => "categories",
         ]);
     }
 
@@ -55,7 +64,8 @@ class HomeController extends Controller
         ])->withCount('products')->get();
 
         return view('shop.brand')->with([
-            'brands' => $brands
+            'brands' => $brands,
+            "page" => "brands",
         ]);
     }
 }
---
+++ app/Http/Controllers/ShopModule/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\ShopModule;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function showBrandProducts($brand_id)
+    {
+        $products = Product::with([
+            'categories',
+            'tags',
+            'image',
+            'gallery'
+        ])
+        ->where('brand_id','=',$brand_id)->get();
+
+        return view("shop.product.index")->with([
+            "products" => $products
+        ]);
+    }
+}
